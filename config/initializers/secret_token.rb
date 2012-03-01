57d7159d0c0e390a5095aa7c9128f3a00323c6de
Lucas Willett <lucas@developingego.com>
2012-03-01 14:57:33 -0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Winloss::Application.config.secret_token = '4aee39133ea1ac8f414fda6f675d064144f1564be177802c8c567126a736feb14d82a370c04a7f8069c12fb3dfb7b03288cea00fb7238fb5fb5442d4337acaf6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Winloss::Application.config.session_store :cookie_store, key: '_winloss_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Winloss::Application.config.session_store :active_record_store
