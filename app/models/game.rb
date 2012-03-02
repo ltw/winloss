96d81bc8a907de6c9e842d0d09ece71247211c41
Lucas Willett <lucas@developingego.com>
2012-03-02 09:34:19 -0800

    Add parsing and tests to game.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,5 @@
+require 'parser'
+
 class Game < ActiveRecord::Base
   belongs_to :winner, :class_name => 'User'
   belongs_to :loser, :class_name => 'User'
@@ -5,4 +7,22 @@ class Game < ActiveRecord::Base
   validates :winner_id, :loser_id, :presence => true
   validates :winner_score, :loser_score, :presence => true
   validates :played_date, :presence => true
+
+  def self.parse phrase
+    new Parser.parse phrase
+  end
+
+  def self.parse! phrase
+    parse(phrase).save!
+  end
+
+  protected
+
+  # allow submission of a string to the winner and loser setter
+  [:winner, :loser].each do |attribute|
+    define_method "#{attribute}=" do |value|
+      value = User.find_or_create_by_name value unless value.respond_to? :name
+      super(value)
+    end
+  end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,4 +1,22 @@
 require 'spec_helper'
 
 describe Game do
+  context 'parsing' do
+    subject { described_class }
+    it { should respond_to :parse }
+    it { should respond_to :parse! }
+
+    # TODO: make this not require two DB writes when creating users
+    it 'should return a valid game when parsing a correct string' do
+      Game.parse('Odin beat Lucas 21-7').should be_valid
+    end
+
+    it 'should try to save on parse!' do
+      game = double('game')
+      Game.stub(:parse).and_return game
+      game.should_receive(:save!).once
+
+      Game.parse!('Odin beat Lucas 21-7')
+    end
+  end
 end
