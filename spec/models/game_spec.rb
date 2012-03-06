e147d3dac95f9aa6f14bcb91fb4313199997d931
Lucas Willett <lucas@developingego.com>
2012-03-06 08:57:21 -0600

    Update spec names to be more declarative.

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -7,11 +7,11 @@ describe Game do
     it { should respond_to :parse! }
 
     # TODO: make this not require two DB reads when creating users
-    it 'should return a valid game when parsing a correct string' do
+    it 'returns a valid game when parsing a correct string' do
       Game.parse('Odin beat Lucas 21-7').should be_valid
     end
 
-    it 'should try to save on parse!' do
+    it 'tries to save on parse!' do
       game = double('game')
       Game.stub(:parse).and_return game
       game.should_receive(:save!).once
