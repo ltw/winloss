23208e03c828d275cd45a9a62b106030ee1a8b00
Lucas Willett <lucas@developingego.com>
2012-03-06 08:47:26 -0600

    Refactor the parser specs to be more clear.

--- spec/lib/parser_spec.rb
+++ spec/lib/parser_spec.rb
@@ -1,36 +1,16 @@
 require 'parser'
 
 describe Parser do
-  [
-    {
-      test: 'a standard layout',
-      phrase: 'Odin beat Lucas 21-7',
-      expected_values: ['Odin', 'Lucas', 21, 7, Date.today],
-    },
-    {
-      test: 'a negative adjective',
-      phrase: 'Odin was beaten by Lucas 14-21',
-      expected_values: ['Lucas', 'Odin', 21, 14, Date.today],
-    },
-    {
-      test: 'a relative date',
-      phrase: 'Odin beat Lucas 21-15 yesterday',
-      expected_values: ['Odin', 'Lucas', 21, 15, (Time.now.utc.to_date - 1)],
-    },
-    {
-      test: 'a specific date',
-      phrase: 'Lucas beat Odin 21-20 2012-02-01',
-      expected_values: ['Lucas', 'Odin', 21, 20, Date.parse('2012-02-01')],
-    },
-    {
-      test: 'an unusual adjective',
-      phrase: 'Odin obliterated Lucas 21-5',
-      expected_values: ['Odin', 'Lucas', 21, 5, Date.today]
-    }
-  ].each do |test_case|
-    it "should parse phrase with #{test_case[:test]}" do
-      Parser.parse(test_case[:phrase]).values.each_with_index do |v, i|
-        test_case[:expected_values][i].should == v
+  {
+    'Odin beat Lucas 21-7'             => ['Odin',  'Lucas', 21,  7, Date.today],
+    'Odin was beaten by Lucas 14-21'   => ['Lucas', 'Odin',  21, 14, Date.today],
+    'Odin beat Lucas 21-15 yesterday'  => ['Odin',  'Lucas', 21, 15, (Time.now.utc.to_date - 1)],
+    'Lucas beat Odin 21-20 2012-02-01' => ['Lucas', 'Odin',  21, 20, Date.parse('2012-02-01')],
+    'Odin obliterated Lucas 21-5'      => ['Odin',  'Lucas', 21,  5, Date.today]
+  }.each do |test_case|
+    it "parses '#{test_case[0]}'" do
+      Parser.parse(test_case[0]).values.each_with_index do |v, i|
+        test_case[1][i].should == v
       end
     end
   end
