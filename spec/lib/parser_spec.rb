15d7dbd28dae08decafb702c9014af7cbadede25
Lucas Willett <lucas@developingego.com>
2012-03-06 10:37:07 -0600

    Refactor specs even more, because I'm on holidays.

--- spec/lib/parser_spec.rb
+++ spec/lib/parser_spec.rb
@@ -7,11 +7,15 @@ describe Parser do
     'Odin beat Lucas 21-15 yesterday'  => ['Odin',  'Lucas', 21, 15, (Time.now.utc.to_date - 1)],
     'Lucas beat Odin 21-20 2012-02-01' => ['Lucas', 'Odin',  21, 20, Date.parse('2012-02-01')],
     'Odin obliterated Lucas 21-5'      => ['Odin',  'Lucas', 21,  5, Date.today]
-  }.each do |test_case|
-    it "parses '#{test_case[0]}'" do
-      Parser.parse(test_case[0]).values.each_with_index do |v, i|
-        test_case[1][i].should == v
-      end
+  }.each do |phrase, values|
+    context "parsing '#{phrase}'" do
+      subject { Parser.parse(phrase) }
+
+      its([:winner])       { should == values[0] }
+      its([:loser])        { should == values[1] }
+      its([:winner_score]) { should == values[2] }
+      its([:loser_score])  { should == values[3] }
+      its([:played_date])  { should == values[4] }
     end
   end
 
