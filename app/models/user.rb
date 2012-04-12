8570a3dcd53964876c8ba51bfd9b35beb972fe78
Lucas Willett <lucas@developingego.com>
2012-04-12 15:25:05 +1000

    Add User#scores.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  def scores
+    connection.select_rows(<<-SQL
+      SELECT    (CASE WHEN winner_id = #{self.id} THEN winner_score ELSE loser_score END) as score
+      FROM      games
+      WHERE     winner_id = #{self.id}
+      OR        loser_id = #{self.id}
+    SQL
+    ).flatten.map(&:to_i)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 describe User do
+  context 'scores' do
+    let(:winner) { User.create! :name => 'Lucas' }
+    let(:loser)  { User.create! :name => 'Odin' }
+    let!(:games) do
+      [5,7,8].map {|num| Game.create! :winner => winner, :loser => loser, :winner_score => 21, :loser_score => num, :played_date => Date.today }
+    end
+
+    it 'should return scores' do
+      loser.scores.should eq [5,7,8]
+    end
+  end
 end
