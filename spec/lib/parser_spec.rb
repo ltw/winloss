476c77d885593277e93d70d5e769ec6e65de707a
Lucas Willett <lucas@developingego.com>
2012-04-27 13:10:11 +1000

    Add parsing specs for team games.

--- spec/lib/parser_spec.rb
+++ spec/lib/parser_spec.rb
@@ -22,4 +22,21 @@ describe Parser do
   it 'will not parse stupid input' do
     Parser.parse('Odin wins too much.').should be_nil
   end
+
+  {
+    'Adrian + Odin beat Lucas + Nate 21-7'            => ['Adrian + Odin',  'Lucas + Nate', 21,  7, Date.today],
+    'Adrian + Odin were beaten by Lucas + Nate 14-21' => ['Lucas + Nate', 'Adrian + Odin',  21, 14, Date.today],
+    'Nate + Odin beat Adrian + Lucas 21-15 yesterday' => ['Nate + Odin',  'Adrian + Lucas', 21, 15, (Time.now.utc.to_date - 1)],
+    'Lucas + Tate beat Nate + Odin 21-20 2012-02-01'  => ['Lucas + Tate', 'Nate + Odin',  21, 20, Date.parse('2012-02-01')],
+  }.each do |phrase, values|
+    context "parsing '#{phrase}'" do
+      subject { Parser.parse(phrase) }
+
+      its([:winner_name])  { should == values[0] }
+      its([:loser_name])   { should == values[1] }
+      its([:winner_score]) { should == values[2] }
+      its([:loser_score])  { should == values[3] }
+      its([:played_date])  { should == values[4] }
+    end
+  end
 end
