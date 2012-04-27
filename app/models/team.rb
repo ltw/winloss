50aec74b69ede5fe0feecf857fc6f19501712ae7
Lucas Willett <lucas@developingego.com>
2012-04-27 13:09:08 +1000

    Add score functionality for teams.

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,4 +5,22 @@ class Team < ActiveRecord::Base
   def set_team_name
     self.name ||= self.users.map(&:name).sort.join(' + ')
   end
+
+  def games
+    TeamGame.for_team(self)
+  end
+
+  def points_per_game
+    scores.empty? ? 0 : scores.reduce(0, :+).to_f / scores.length
+  end
+
+  def scores
+    connection.select_rows(<<-SQL
+      SELECT    (CASE WHEN winner_id = #{self.id} THEN winner_score ELSE loser_score END) as score
+      FROM      team_games
+      WHERE     winner_id = #{self.id}
+      OR        loser_id = #{self.id}
+    SQL
+    ).flatten.map(&:to_i)
+  end
 end
