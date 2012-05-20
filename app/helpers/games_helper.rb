b062956c0903a3a8cae71da3b28b0f3f8bae9f78
Lucas Willett <lucas@developingego.com>
2012-05-20 21:30:53 +1000

    Add ranking_for and PPG helpers to GameHelper.

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -11,4 +11,21 @@ module GamesHelper
       content
     end
   end
+
+  def ranking_for(user, date_range)
+    games = date_range ? Game.where(played_date: date_range) : Game.scoped
+    scores = games.scores_for(user)
+    if scores.present?
+      content = content_tag :span, number_with_precision(points_per_game(scores), precision: 2), :class => 'points-per-game'
+      content += ' / '
+      content += content_tag :span, scores.count, :class => 'count'
+      content
+    else
+      content_tag :span, '--', :class => 'points-per-game'
+    end
+  end
+
+  def points_per_game(scores)
+    scores.empty? ? 0 : scores.reduce(0, :+).to_f / scores.length
+  end
 end
