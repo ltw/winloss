d5fb4a109fdf54aaa8fa143790f234959bfbee0d
Lucas Willett <lucas@developingego.com>
2012-05-21 21:08:58 +1000

    Add flash messages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_messages_for(*types)
+    types.map do |type|
+      content_tag :div, {:class => "alert alert-block alert-#{type}"}, false do
+        close_link = content_tag :a, raw('&times;'), {:class => 'close', :'data-dismiss' => 'alert'}
+        close_link + content_tag(:div, flash[type], {:class => "container"}, false)
+      end if flash[type].present?
+    end.compact.join.html_safe
+  end
 end
