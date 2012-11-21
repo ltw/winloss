4932ae4d60206c850375028862a3bb787d80b4ce
Lucas Willett <lucas@groupon.com>
2012-11-20 23:44:57 -0600

    Add self.top_ranked to User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :teams
   validates :name, presence: true, uniqueness: true
 
+  def self.top_ranked(count=nil)
+    users = all.sort {|x,y| y.points_per_game <=> x.points_per_game }
+    users = users.first(count) if count
+    users
+  end
+
   def games
     Game.for_user(self)
   end
