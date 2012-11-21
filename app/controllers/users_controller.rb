598d266a5d7c3b5659fae8e401f857970f69636a
Lucas Willett <lucas@groupon.com>
2012-11-20 23:59:20 -0600

    Make users return signature use a top level collection.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   respond_to :json
   def index
     count = params[:count]
-    respond_with(User.top_ranked((count && count.to_i)))
+    respond_with({:users => User.top_ranked((count && count.to_i))})
   end
 end
